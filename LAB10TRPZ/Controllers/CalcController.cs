4b95cee2f38fd586c1a3befdbee2eef20df01968
Deda3d <alekspains8@gmail.com>
2024-11-22 17:52:23 +0100

    Добавьте файлы проекта.

---
+++ LAB10TRPZ/Controllers/CalcController.cs
@@ -0,0 +1,52 @@
+﻿using LAB10TRPZ.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB10TRPZ.Controllers
+{
+    public class CalcController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string array1, string array2)
+        {
+            string result;
+
+            try
+            {
+                // Перетворення введених даних у масиви
+                int[] parsedArray1 = array1?.Split(',').Select(int.Parse).ToArray() ?? new int[0];
+                int[] parsedArray2 = array2?.Split(',').Select(int.Parse).ToArray() ?? new int[0];
+
+                // Обчислення сум
+                int sum1 = parsedArray1.Sum();
+                int sum2 = parsedArray2.Sum();
+
+                if (sum1 > sum2)
+                {
+                    result = $"Сума елементів першого масиву ({sum1}) більша, ніж другого масиву ({sum2}).";
+                }
+                else if (sum1 < sum2)
+                {
+                    result = $"Сума елементів другого масиву ({sum2}) більша, ніж першого масиву ({sum1}).";
+                }
+                else
+                {
+                    result = $"Сума елементів обох масивів однакова ({sum1}).";
+                }
+            }
+            catch (Exception e)
+            {
+                result = "Виникла помилка: " + e.Message;
+            }
+
+            ViewBag.Result = result;
+            return View();
+        }
+
+    }
+}
---
+++ LAB10TRPZ/Models/OperandsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LAB10TRPZ.Models
+{
+    public class OperandsModel
+    {
+        public int[] Array1 { get; set; }
+        public int[] Array2 { get; set; }
+    }
+}
